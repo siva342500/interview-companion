d52b228a82971880cff1973376487aeb88e0462f
Sivaparvathamma <dnagendra@prazo.in>
2024-10-16 12:48:35 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'interview_campaign',
+  webDir: 'www'
+};
+
+export default config;
